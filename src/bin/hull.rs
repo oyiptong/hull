e5c7bbef796e4b9d086d871470349b9380f7ea74
Olivier Yiptong <olivier.yiptong@airbnb.com>
2016-12-23 00:05:24 -0800

    move some logic to lib, add unit tests

--- src/bin/hull.rs
+++ src/bin/hull.rs
@@ -1,63 +1,10 @@
 use std::env;
-use std::time::Duration;
-use std::path::{Path, PathBuf};
 use std::time::Instant;
-use std::io::{self, Write};
 use std::process::{exit, Command};
 #[macro_use] extern crate log;
 extern crate env_logger;
+extern crate hull;
 
-
-fn stderr_write(message :String) -> io::Result<()> {
-    let stderr = io::stderr();
-    let mut handle = stderr.lock();
-
-    try!(handle.write(message.as_bytes()));
-    try!(handle.write(b"\n"));
-    Ok(())
-}
-
-/// Returns a shell PATH environment string minus a given directory
-fn remove_dir_from_path(dir :String, path_str :String) -> String{
-    let mut child_paths = Vec::new(); 
-    for path in path_str.split(":") {
-        if path != dir {
-            child_paths.push(path);
-        }
-    }
-
-    return child_paths.join(":");
-}
-
-
-/// Returns whether a command and hull's binary paths are the same
-/// # Panics
-/// Will panic if the command path is relative and does not resolve
-fn paths_equivalent(binary_path :PathBuf, cur_cmd :String, cur_path :&Path) -> bool {
-    let command_path;
-
-    if cur_cmd.contains("/") {
-        let mut cur_path_buf = cur_path.to_path_buf();
-        cur_path_buf.push(cur_cmd);
-        command_path = cur_path_buf.canonicalize().unwrap();
-    } else {
-        command_path = PathBuf::from(cur_cmd);
-    }
-
-    debug!("comparing command_path:'{}' binary_path:'{}'",
-            command_path.display(),
-            binary_path.display()
-    );
-
-    let binary_path = binary_path.canonicalize().unwrap();
-
-    return command_path == binary_path;
-}
-
-fn duration_in_millis(duration :Duration) -> f64 {
-    return ((duration.as_secs() as f64) * 1_000.0) +
-        ((duration.subsec_nanos() as f64) / 1_000_000.0);
-}
 
 fn main() {
     let now = Instant::now();
@@ -80,15 +27,17 @@ fn main() {
     let cur_dir = env::current_dir().unwrap();
     let cur_path = cur_dir.as_path();
 
-    if paths_equivalent(binary_path, cmd.clone(), cur_path) {
-        error!("Error: Unwilling to run program recursively. Please check your paths.");
+    if hull::cmd::paths_equivalent(binary_path, cmd.clone(), cur_path) {
+        hull::cmd::stderr_write(String::from("Error: Unwilling to run program recursively.")
+                                + "Please check your paths."
+        ).unwrap();
         exit(1);
     }
 
     debug!("command: '{}' args: '{:#?}'", cmd, cmd_args);
 
     let path_str = env::var("PATH").unwrap();
-    let new_path_str = remove_dir_from_path(binary_dir, path_str);
+    let new_path_str = hull::cmd::remove_dir_from_path(binary_dir, path_str);
 
     let boot_duration = now.elapsed();
 
@@ -111,13 +60,13 @@ fn main() {
     } else {
         let err = result.unwrap_err();
         let error_code = err.raw_os_error().unwrap();
-        stderr_write(format!("{} : {}", cmd, err.to_string())).unwrap();
+        hull::cmd::stderr_write(format!("{} : {}", cmd, err.to_string())).unwrap();
         exit(error_code);
     }
 
     let total_duration = now.elapsed();
 
-    let boot_time = duration_in_millis(boot_duration);
-    let total_time = duration_in_millis(total_duration);
-    info!("boot_time: {} ms\ntotal_time: {} ms", boot_time, total_time);
+    let boot_time = hull::cmd::duration_in_millis(boot_duration);
+    let total_time = hull::cmd::duration_in_millis(total_duration);
+    info!("boot_time: {} ms total_time: {} ms", boot_time, total_time);
 }
